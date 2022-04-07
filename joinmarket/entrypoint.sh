ba20d60e4a8da2692d345305f3b76f465fea76e1
roshii <roshii@riseup.net>
2022-04-07 11:38:45 +0200

    cp

--- joinmarket/entrypoint.sh
+++ joinmarket/entrypoint.sh
@@ -5,6 +5,6 @@ datadir="/root/.joinmarket"
 file_name="joinmarket.cfg"
 target="${datadir}/${file_name}"
 
-! [ -f "$target" ] && mv "/etc/${file_name}" "$target"
+! [ -f "$target" ] && cp "/etc/${file_name}" "$target"
 
 python "$@"

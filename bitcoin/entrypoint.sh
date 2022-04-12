ca04ce81a5f2c8c4ab0a6de2a4762d1665cd90bd
roshii <roshii@riseup.net>
2022-04-12 15:29:23 +0200

    set datadir by command-line arg

--- bitcoin/entrypoint.sh
+++ bitcoin/entrypoint.sh
@@ -5,25 +5,22 @@ user="bitcoin"
 
 # Copy default config if none is set
 datadir="/srv/data"
-confdir="/home/${user}/.bitcoin"
 file_name="bitcoin.conf"
-target="${datadir}/${file_name}"
 
-
-if ! [ -f "$target" ]; then
+if ! [ -f "${datadir}/${file_name}" ]; then
   cp "/etc/${file_name}" "$datadir"
-  mkdir -p "${confdir}"
-  ln -sf "$target" "${confdir}/${file_name}"
 fi
 
 chown -R "$user" /var/lib/bitcoin
 chown -R "$user" /srv
 
 # Create joinmarket wallet is None is present
-walletdir="/srv/wallet"
 wallet="joinmarket"
+walletdir="/srv/wallet"
 
 ! [ -d "${walletdir}/${wallet}" ] \
 && su-exec "$user" bitcoin-wallet -datadir="$walletdir" -wallet="$wallet" create
 
-su-exec "$user" "$@"
+cmd="$1"
+shift
+su-exec "$user" "$cmd" -datadir="$datadir" "$@"

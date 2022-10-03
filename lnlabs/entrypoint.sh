9691a2f3b534e006de6a5f4c700745d555efed1d
roshii <roshii@riseup.net>
2022-10-03 09:53:06 +0200

    stash

---
+++ lnlabs/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/ash
+set -e
+
+CONFIG_FILE="${LNLABS_SERVICE}d.conf"
+DATADIR="/home/${LNLABS_SERVICE}/.${LNLABS_SERVICE}"
+NETWORK="mainnet"
+CONFIGDIR="${DATADIR}/${NETWORK}"
+
+# Copy default config if none is found
+if ! [ -f "${CONFIGDIR}/${CONFIG_FILE}" ]; then
+  mkdir -p "$CONFIGDIR"
+  cp "/etc/${CONFIG_FILE}" "$CONFIGDIR"
+  chown -R "$LNLABS_SERVICE" "$DATADIR"
+fi
+
+exec su-exec "$LNLABS_SERVICE" "$@"

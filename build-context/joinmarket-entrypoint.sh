5ec7b0b011c4a7a198f0f71c5a5fd572af7566b2
roshii <roshii@riseup.net>
2022-11-06 17:45:00 +0100

    use ed25519 curve

--- build-context/joinmarket-entrypoint.sh
+++ build-context/joinmarket-entrypoint.sh
@@ -5,8 +5,8 @@ SSLDIR="/home/satoshi/.joinmarket/ssl"
 
 if [ ! -e "${SSLDIR}/key.pem" ] ; then
   mkdir -p "$SSLDIR"
-  openssl ecparam -name secp256k1 -genkey -noout -out "${SSLDIR}/key.pem"
-  openssl ec -in "${SSLDIR}/key.pem" -pubout -out "${SSLDIR}/pub.pem"
+  openssl genpkey -algorithm ed25519 -out "${SSLDIR}/key.pem"
+  openssl pkey -in "${SSLDIR}/key.pem" -out "${SSLDIR}/pub.pem"
   openssl req -new -x509 -key "${SSLDIR}/key.pem" -out "${SSLDIR}/cert.pem" -days 360 -nodes \
     -subj "/CN=joinmarket"
 fi
